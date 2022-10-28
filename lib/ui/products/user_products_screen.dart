48f8f58eb7816bf48c721d551d3a8be23e86cce7
TranThiKimLinhB1910402 <kimlinhtr@gmail.com>
2022-10-28 22:24:49 +0700

    Luu danh muc san pham vao Realtime Database cua Firebase

--- lib/ui/products/user_products_screen.dart
+++ lib/ui/products/user_products_screen.dart
@@ -8,23 +8,35 @@ import 'package:provider/provider.dart';
 class UserProductScreen extends StatelessWidget {
   static const routeName = '/user-products';
   const UserProductScreen({super.key});
+  Future<void> _refreshProducts(BuildContext context) async {
+    await context.read<ProductsManager>().fetchProducts(true);
+  }
 
   @override
   Widget build(BuildContext context) {
     final productsManager = ProductsManager();
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Your Products'),
-        actions: <Widget>[
-          buildAddButton(context),
-        ],
-      ),
-      drawer: const AppDrawer(),
-      body: RefreshIndicator(
-        onRefresh: () async => debugPrint('refresh products'),
-        child: buildUserProductListView(),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Your Products'),
+          actions: <Widget>[
+            buildAddButton(context),
+          ],
+        ),
+        drawer: const AppDrawer(),
+        body: FutureBuilder(
+          future: _refreshProducts(context),
+          builder: (ctx, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return RefreshIndicator(
+              onRefresh: () async => debugPrint('refresh products'),
+              child: buildUserProductListView(),
+            );
+          },
+        ));
   }
 
   Widget buildUserProductListView() {
